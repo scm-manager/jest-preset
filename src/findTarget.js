4181d1a0a3dd986df057d8b69ab6e2642654e5c9
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-11-16 21:19:42 +0100

    Support gradle based projects

--- src/findTarget.js
+++ src/findTarget.js
@@ -24,16 +24,16 @@
 const fs = require("fs");
 const path = require("path");
 
-function findMavenModuleRoot(directory) {
-  if (fs.existsSync(path.join(directory, "pom.xml"))) {
-    return directory;
-  }
-  return findMavenModuleRoot(path.resolve(directory, ".."));
-}
-
 function findTarget(directory) {
-  const moduleRoot = findMavenModuleRoot(directory);
-  return path.join(moduleRoot, "target");
+  if (fs.existsSync(path.join(directory, "build.gradle"))) {
+    return path.join(directory, "build");
+  }
+
+  if (fs.existsSync(path.join(directory, "pom.xml"))) {
+    return path.join(directory, "target");
+  }
+
+  return findTarget(path.resolve(directory, ".."));
 }
 
 module.exports = findTarget;
